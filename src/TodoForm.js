77a82b8cca72ff858947d699be79cb8530747fba
Annabel Wilmerding <awilmerding@NYC-US16685-MBP.local>
2019-12-04 13:05:29 -0800

    all components set up

---
+++ src/TodoForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class TodoForm extends React.Component {
+
+  render() {
+    return (
+
+      <div>
+        Todos
+      </div>
+    );
+  }
+}
+
+export default TodoForm;
---
+++ src/Todos.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Todos extends React.Component {
+
+  render() {
+    return (
+
+      <div>
+        Todos
+      </div>
+    );
+  }
+}
+
+export default Todos;
